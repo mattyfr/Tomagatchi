cfe27558b94ede4f22e0dcb48b8671bbb4147651
Mathias Granlund <granlund.jr@gmail.com>
2026-09-03 13:44:45 +0200

    2

--- ConsoleApp1/Class.cs
+++ ConsoleApp1/Class.cs
@@ -7,8 +7,66 @@ public class Tamagotchi
 {
     public int hunger = 0;
     public int boredom = 0;
-    public List<String> words = new List<string>{};
+    public List<String> words = new List<string> { };
     public bool isAlive = true;
     public string name = "";
     public int preferedFood;
+    public static Tamagotchi NextDay(Tamagotchi tamagotchi)
+    {
+        tamagotchi.hunger += Random.Shared.Next(0, 3);
+        tamagotchi.boredom += Random.Shared.Next(0, 3);
+        if (tamagotchi.hunger > 10 || tamagotchi.boredom > 10)
+        {
+            tamagotchi.isAlive = false;
+        }
+        return tamagotchi;
+    }
+    public static Tamagotchi feed(Tamagotchi tamagotchi)
+    {
+        Console.WriteLine("What do you want to feed your tamagotchi. Write the number");
+        Console.WriteLine("1: appel \n 2: meat \n 3: smoothie");
+        int Choice;
+        while (!int.TryParse(Console.ReadLine(), out Choice))
+        {
+            Console.WriteLine("try again");
+        }
+        if (Choice == 1)
+        {
+            Console.WriteLine("You feed your tamagatchi appel");
+            if (tamagotchi.preferedFood == Choice)
+            {
+                tamagotchi.hunger -= 3;
+            }
+            else
+            {
+                tamagotchi.hunger -= 2;
+            }
+        }
+        if (Choice == 2)
+        {
+            Console.WriteLine("You feed your tamagatchi meat");
+            if (tamagotchi.preferedFood == Choice)
+            {
+                tamagotchi.hunger -= 3;
+            }
+            else
+            {
+                tamagotchi.hunger -= 2;
+            }
+        }
+        if (Choice == 3)
+        {
+            Console.WriteLine("You feed your tamagatchi smoothie");
+            if (tamagotchi.preferedFood == Choice)
+            {
+                tamagotchi.hunger -= 3;
+            }
+            else
+            {
+                tamagotchi.hunger -= 2;
+            }
+        }
+        return tamagotchi;
+    }
+    
 }
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,7 +9,7 @@ int money = 0;
 while (tamagotchi.isAlive)
 {
     WriteStats(tamagotchi, money);
-    tamagotchi = NextDay(tamagotchi);
+    tamagotchi = Tamagotchi.NextDay(tamagotchi);
     (money, tamagotchi) = GetCommand(money, tamagotchi);
     Console.ReadLine();
 }
@@ -17,25 +17,13 @@ static void WriteStats(Tamagotchi tamagotchi, int money)
 {
     Console.WriteLine($"Hunger: {tamagotchi.hunger} \n Boredom: {tamagotchi.boredom} \n Moeny: {money}");
 }
-static Tamagotchi NextDay(Tamagotchi tamagotchi)
-{
-    int plusHunger = Random.Shared.Next(0,3);
-    int plusBoredom = Random.Shared.Next(0,3);
-    tamagotchi.hunger += plusHunger;
-    tamagotchi.boredom += plusBoredom;
-    if (tamagotchi.hunger > 10 || tamagotchi.boredom > 10)
-    {
-        tamagotchi.isAlive = false;
-    }
-    return tamagotchi;
-}
 static (int,Tamagotchi) GetCommand(int money, Tamagotchi tamagotchi)
 {
     string command = Console.ReadLine();
     if (command == "feed" && money > 1)
     {
         money--;
-        tamagotchi = feed(tamagotchi);
+        tamagotchi = Tamagotchi.feed(tamagotchi);
     }
     else if (command == "feed" && money < 1)
     {
@@ -51,51 +39,4 @@ static (int,Tamagotchi) GetCommand(int money, Tamagotchi tamagotchi)
         money += 4;
     }
     return (money, tamagotchi);
-}
-static Tamagotchi feed(Tamagotchi tamagotchi)
-{
-    Console.WriteLine("What do you want to feed your tamagotchi. Write the number");
-    Console.WriteLine("1: appel \n 2: meat \n 3: smoothie");
-    int Choice;
-    while (!int.TryParse(Console.ReadLine(), out Choice))
-    {
-        Console.WriteLine("try again");
-    }
-    if (Choice == 1)
-    {
-        Console.WriteLine("You feed your tamagatchi appel");
-        if (tamagotchi.preferedFood == Choice)
-        {
-            tamagotchi.hunger -=3;
-        }
-        else
-        {
-            tamagotchi.hunger -=2;
-        }
-    }
-    if (Choice == 2)
-    {
-        Console.WriteLine("You feed your tamagatchi meat");
-        if (tamagotchi.preferedFood == Choice)
-        {
-            tamagotchi.hunger -=3;
-        }
-        else
-        {
-            tamagotchi.hunger -=2;
-        }
-    }
-    if (Choice == 3)
-    {
-        Console.WriteLine("You feed your tamagatchi smoothie");
-        if (tamagotchi.preferedFood == Choice)
-        {
-            tamagotchi.hunger -=3;
-        }
-        else
-        {
-            tamagotchi.hunger -=2;
-        }
-    }
-    return tamagotchi;
 }
